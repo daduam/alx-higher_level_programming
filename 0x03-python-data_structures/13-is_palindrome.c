82be3239eeadbabedcc1902b59bcb3092c298fe5
Joseph Ampadu <josephampadu549@gmail.com>
2023-01-08 10:01:01 +0000

    use recursion for list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * palindrome_util - checks if a sublist is a palindrome.
+ *
+ * @head: the address of the head current sublist.
+ * @p: the address of the current element to compare.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+int palindrome_util(listint_t **head, listint_t *p)
+{
+	if (p == NULL)
+		return (1);
+	if (palindrome_util(head, p->next) && (*head)->n == p->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome.
  *
  * @head: the address of the head of the singly linked list.
@@ -8,30 +27,7 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *p1, *p2, *current, *next;
-
 	if (head == NULL || *head == NULL)
 		return (1);
-
-	p2 = NULL;
-	current = *head;
-	next = NULL;
-	while (current)
-	{
-		next = current->next;
-		current->next = p2;
-		p2 = current;
-		current = next;
-	}
-
-	p1 = *head;
-	while (p1 && p2)
-	{
-		if (p1->n != p2->n)
-			return (0);
-		p1 = p1->next;
-		p2 = p2->next;
-	}
-
-	return (1);
+	return (palindrome_util(head, *head));
 }
