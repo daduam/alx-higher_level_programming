ae9d68c664dd9ba12226f643f25c8ec01b33f09d
Joseph Ampadu <josephampadu549@gmail.com>
2023-01-24 11:14:23 +0000

    (0x06) add advanced tasks

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+class Node:
+    """Defines a node of a singly linked list"""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a node of a singly linked list
+
+        Args:
+            data (int): Data.
+            next_node (:obj:`Node`, optional): Next linkedlist node.
+        """
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """Getter for data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Setter for data"""
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Getter for next_node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Setter for next_node"""
+        if value is not None and type(value) is not Node:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Defines a singly linked list"""
+
+    def __init__(self):
+        """Initializes the head of the singly linked list"""
+        self.__head = None
+
+    def __str__(self):
+        """String representation of singly linked list"""
+        result = ""
+        current = self.__head
+        while current is not None:
+            result += "{:d}".format(current.data)
+            current = current.next_node
+            if current is not None:
+                result += "\n"
+        return result
+
+    def sorted_insert(self, value):
+        """Inserts a new node into the correct sorted position in the list"""
+        current = self.__head
+        new_node = Node(value)
+        if current is None:
+            self.__head = new_node
+        elif current.data > value:
+            new_node.next_node = current
+            self.__head = new_node
+        else:
+            while current.next_node is not None:
+                if current.next_node.data > value:
+                    break
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize the square with a size.
+
+        Args:
+            size (:obj:`int`, optional): Size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the current square area.
+
+        Returns:
+            The current square area.
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """Getter for size.
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        """== comparison operator for square"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """!= comparison operator for square"""
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """> comparison operator for square"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """>= comparison operator for square"""
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        """< comparison operator for square"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """<= comparison operator for square"""
+        return self.area() <= other.area()
