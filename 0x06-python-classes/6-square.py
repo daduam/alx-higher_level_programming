c820104251c4eb4c2c2b489e9f9b16d368cd4c32
Joseph Ampadu <josephampadu549@gmail.com>
2023-01-24 13:31:19 +0000

    (0x06) what??

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,11 +46,14 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for y in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
 
     @property
     def position(self):
