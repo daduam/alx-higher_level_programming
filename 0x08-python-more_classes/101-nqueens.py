2d3a4bb1d910c14962f9e7433590cd2624ece782
Joseph Ampadu <josephampadu549@gmail.com>
2023-01-30 14:53:22 +0000

    (0x08) refactor nqueens solution

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,18 +3,20 @@
 import sys
 
 
-def print_error_and_exit(msg="", status=0):
+def print_error_and_exit(msg="", status=1):
     """Prints an error and exit with status"""
     print(msg)
     sys.exit(status)
 
 
 def check_usage():
+    """Checks and prints usage"""
     if len(sys.argv) != 2:
         print_error_and_exit("Usage: nqueens N")
 
 
 def get_n() -> int:
+    """Get number of queens from arguments"""
     try:
         n = int(sys.argv[1])
         if n < 4:
@@ -25,6 +27,7 @@ def get_n() -> int:
 
 
 def queens(n, i, a, b, c):
+    """Generator for nqueens problem"""
     if i < n:
         for j in range(n):
             if j not in a and i+j not in b and i-j not in c:
@@ -33,12 +36,8 @@ def queens(n, i, a, b, c):
         yield a
 
 
-def main():
+if __name__ == "__main__":
     check_usage()
     N = get_n()
     for solution in queens(N, 0, [], [], []):
         print([[x, solution[x]] for x in range(N)])
-
-
-if __name__ == "__main__":
-    main()
