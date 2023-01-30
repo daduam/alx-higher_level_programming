90885998a0dbee3d449542affbb7706844b10ad8
Joseph Ampadu <josephampadu549@gmail.com>
2023-01-30 14:49:13 +0000

    (0x08) add nqueens task

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""N Queens"""
+import sys
+
+
+def print_error_and_exit(msg="", status=0):
+    """Prints an error and exit with status"""
+    print(msg)
+    sys.exit(status)
+
+
+def check_usage():
+    if len(sys.argv) != 2:
+        print_error_and_exit("Usage: nqueens N")
+
+
+def get_n() -> int:
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print_error_and_exit("N must be at least 4")
+        return n
+    except Exception:
+        print_error_and_exit("N must be a number")
+
+
+def queens(n, i, a, b, c):
+    if i < n:
+        for j in range(n):
+            if j not in a and i+j not in b and i-j not in c:
+                yield from queens(n, i+1, a+[j], b+[i+j], c+[i-j])
+    else:
+        yield a
+
+
+def main():
+    check_usage()
+    N = get_n()
+    for solution in queens(N, 0, [], [], []):
+        print([[x, solution[x]] for x in range(N)])
+
+
+if __name__ == "__main__":
+    main()
