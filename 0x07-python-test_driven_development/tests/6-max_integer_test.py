6d70e88bedf4ef15d5bdc53869cbcd32a8fb969b
Joseph Ampadu <josephampadu549@gmail.com>
2023-01-31 11:12:28 +0000

    (0x07) add tests for task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Defines tests for max_integer"""
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+        self.assertIsNone(max_integer())
+
+    def test_max_integer(self):
+        self.assertEquals(max_integer([1, 2, 3, 4]), 4)
+        self.assertEquals(max_integer([2, 3, 4, 1]), 4)
+        self.assertEquals(max_integer([2, 3, -4, 1]), 3)
+        self.assertEquals(max_integer([-2, -3, -4, -1]), -1)
+        self.assertEquals(max_integer([0, -1]), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
