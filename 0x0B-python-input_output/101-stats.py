1e9e94cba952f7876c9092f1e3a7ffc2c02d4f37
Joseph Ampadu <josephampadu549@gmail.com>
2023-02-07 14:03:21 +0000

    check valid status codes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     lineno = 0
     fsize = 0
     status_codes = {}
+    valid_status_codes = ["200", "301", "400",
+                          "401", "403", "404", "405", "500"]
 
     try:
         for line in sys.stdin:
@@ -23,9 +25,10 @@ if __name__ == "__main__":
             try:
                 file_size, status_code = int(line[-1]), line[-2]
                 fsize += file_size
-                if status_code not in status_codes:
-                    status_codes[status_code] = 0
-                status_codes[status_code] += 1
+                if status_code in valid_status_codes:
+                    if status_code not in status_codes:
+                        status_codes[status_code] = 0
+                    status_codes[status_code] += 1
                 if lineno == 10:
                     display_stats(fsize, status_codes)
                     lineno = 0
