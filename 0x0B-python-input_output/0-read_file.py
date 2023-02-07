9260fbb92465ceab701e349db81117efc89664cb
Joseph Ampadu <josephampadu549@gmail.com>
2023-02-07 12:17:13 +0000

    (0x0B) fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,4 @@ def read_file(filename=""):
             Defaults to an empty string.
     """
     with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
