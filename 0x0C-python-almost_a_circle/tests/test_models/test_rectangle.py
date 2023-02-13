7f9520344b9a48c4f776b6915c101d2b379c6598
Joseph Ampadu <josephampadu549@gmail.com>
2023-02-13 22:02:51 +0000

    (0x0C) add unit tests for rectangle constructor

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,6 +14,110 @@ class TestRectangle(unittest.TestCase):
         """Test Rectangle inherits from Base."""
         self.assertTrue(issubclass(Rectangle, Base))
 
+    def test_private_instance_attributes(self):
+        """Test private instance attributes."""
+        r1 = Rectangle(1, 1)
+        self.assertTrue(hasattr(r1, "_Rectangle__width"))
+        self.assertTrue(hasattr(r1, "_Rectangle__height"))
+        self.assertTrue(hasattr(r1, "_Rectangle__x"))
+        self.assertTrue(hasattr(r1, "_Rectangle__y"))
+
+    def test_private_instance_attributes_getters(self):
+        """Test private instance attributes getters."""
+        r1 = Rectangle(2, 3)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 3)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+
+    def test_private_instance_attributes_setters(self):
+        """Test private instance attributes setters."""
+        r1 = Rectangle(2, 3)
+        r1.width = 5
+        r1.height = 7
+        r1.x = 2
+        r1.y = 3
+        self.assertEqual(r1.width, 5)
+        self.assertEqual(r1.height, 7)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 3)
+
+    def test_constructor_wrong_number_of_arguments(self):
+        """Test rectangle constructor with wrong number of arguments"""
+        with self.assertRaises(TypeError):
+            Rectangle()
+        with self.assertRaises(TypeError):
+            Rectangle(1)
+
+    def test_constructor_only_positional_arguments(self):
+        """Test rectangle constructor with only positional arguments."""
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, getattr(Base, "_Base__nb_objects"))
+
+    def test_constructor_with_one_keyword_argument(self):
+        """Test rectangle constructor with one keyword argument."""
+        r1 = Rectangle(1, 2, x=3)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, getattr(Base, "_Base__nb_objects"))
+
+        r2 = Rectangle(1, 2, y=3)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 3)
+        self.assertEqual(r2.id, getattr(Base, "_Base__nb_objects"))
+
+        r3 = Rectangle(1, 2, id=25)
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
+        self.assertEqual(r3.id, 25)
+
+    def test_constructor_with_two_keyword_arguments(self):
+        """Test constructor with two keyword arguments."""
+        r1 = Rectangle(1, 2, x=3, y=4)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 4)
+        self.assertEqual(r1.id, getattr(Base, "_Base__nb_objects"))
+
+        r2 = Rectangle(1, 2, x=3, id=25)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 3)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.id, 25)
+
+        r3 = Rectangle(1, 2, y=3, id=25)
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 3)
+        self.assertEqual(r3.id, 25)
+
+    def test_constructor_with_all_arguments(self):
+        """Test constructor with all arguments."""
+        r1 = Rectangle(1, 2, x=3, y=4, id=25)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 4)
+        self.assertEqual(r1.id, 25)
+        self.assertDictEqual(r1.__dict__, {'id': 25,
+                                           '_Rectangle__width': 1,
+                                           '_Rectangle__height': 2,
+                                           '_Rectangle__x': 3,
+                                           '_Rectangle__y': 4})
+
 
 if __name__ == "__main__":
     unittest.main()
