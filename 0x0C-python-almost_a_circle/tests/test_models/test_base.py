bb3ae0567c59727a780581b233c54b9630785d73
Joseph Ampadu <josephampadu549@gmail.com>
2023-02-14 13:05:33 +0000

    (0x0C) unit tests for to_json_string Base static method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -28,3 +28,22 @@ class TestBase(unittest.TestCase):
         self.assertEqual(self.b1.id, 1)
         self.assertEqual(self.b2.id, 25)
         self.assertDictEqual(self.b1.__dict__, {'id': 1})
+
+    def test_to_json_string_static_method(self):
+        """Test to_json_string static method"""
+        self.assertIn("to_json_string", dir(Base))
+
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+        list_dictionaries = [{"x": 1, "y": 2}]
+        result = Base.to_json_string(list_dictionaries)
+        expected = str(list_dictionaries).replace("'", '"')
+        self.assertEqual(result, expected)
+
+        list_dictionaries = [{"x": 1, "y": 2},
+                             {"x": 1, "y": 2, "width": 3, "height": 4}]
+        result = Base.to_json_string(list_dictionaries)
+        expected = str(list_dictionaries).replace("'", '"')
+        self.assertEqual(result, expected)
