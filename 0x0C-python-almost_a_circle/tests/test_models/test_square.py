671d709957e2a975fd45129b274d013f6761ced8
Joseph Ampadu <josephampadu549@gmail.com>
2023-02-14 01:16:55 +0000

    (0x0C) add tests for Square private attrs

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -19,3 +19,31 @@ class TestSquare(unittest.TestCase):
         """Test Square inheritance"""
         self.assertTrue(issubclass(Square, Rectangle))
         self.assertTrue(issubclass(Square, Base))
+
+    def test_private_instance_attributes(self):
+        """Test private instance attributes."""
+        sq = Square(1)
+        self.assertTrue(hasattr(sq, "_Rectangle__width"))
+        self.assertTrue(hasattr(sq, "_Rectangle__height"))
+        self.assertTrue(hasattr(sq, "_Rectangle__x"))
+        self.assertTrue(hasattr(sq, "_Rectangle__y"))
+
+    def test_private_instance_attributes_getters(self):
+        """Test private instance attributes getters."""
+        sq = Square(1)
+        self.assertEqual(sq.width, 1)
+        self.assertEqual(sq.height, 1)
+        self.assertEqual(sq.x, 0)
+        self.assertEqual(sq.y, 0)
+
+    def test_private_instance_attributes_setters(self):
+        """Test private instance attributes setters."""
+        sq = Square(1)
+        sq.width = 2
+        sq.height = 2
+        sq.x = 3
+        sq.y = 4
+        self.assertEqual(sq.width, 2)
+        self.assertEqual(sq.height, 2)
+        self.assertEqual(sq.x, 3)
+        self.assertEqual(sq.y, 4)
