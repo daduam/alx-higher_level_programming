d9108b6b5b4ef2e88453e592ed732585b48938ba
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-13 21:20:30 +0000

    Select and filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Select states"""
+import MySQLdb
+import sys
+
+host = "localhost"
+port = 3306
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+
+conn = MySQLdb.connect(host=host,
+                       port=port,
+                       user=user,
+                       passwd=passwd,
+                       db=db,
+                       charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
