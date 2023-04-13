e437c4e5ca7c32a0927948030d18ee50b6d9674a
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-13 22:39:59 +0000

    Safe filter states with substitution query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table where name
+matches the supplied argument.
+"""
+import MySQLdb
+import sys
+
+host = "localhost"
+port = 3306
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+
+conn = MySQLdb.connect(host=host,
+                       port=port,
+                       user=user,
+                       passwd=passwd,
+                       db=db,
+                       charset="utf8")
+cur = conn.cursor()
+
+state = sys.argv[4]
+cur.execute(
+    "SELECT * FROM states WHERE name = %s ORDER BY id", (state,))
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
