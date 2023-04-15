f7e1852fb68c59dde44a1837d045efcb74041371
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-15 00:47:24 +0000

    Fix filter states by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """Displays all values in the states table where name matches the argument"""
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    host = "localhost"
-    port = 3306
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-
-    conn = MySQLdb.connect(host=host,
-                           port=port,
-                           user=user,
-                           passwd=passwd,
-                           db=db,
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
     state = sys.argv[4]
-    cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(state))
+    query = """
+    SELECT * FROM states
+    WHERE BINARY name = '{}' ORDER BY states.id ASC
+    """
+    cur.execute(query.format(state))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
