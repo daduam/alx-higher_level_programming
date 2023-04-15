f169923bd822d11f7f48882e3a1fc440d1c55e78
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-15 13:55:42 +0000

    List all city objects

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -13,4 +13,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
-    cities = relationship("City", cascade="all")
+    cities = relationship("City", cascade="all", backref="state")
