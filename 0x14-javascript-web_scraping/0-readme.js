8c37f2728036984f133b5eced7c403bb146c8bd3
Joseph Ampadu <jkampadujnr@gmail.com>
2023-05-23 16:32:39 +0000

    Read and print the contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', function (error, data) {
+  if (error) console.log(error);
+  else console.log(data);
+});
